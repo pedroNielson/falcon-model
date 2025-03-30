3b28cdabadc03b77c21b2da62e0e1d6676a97a85
root <root@DESKTOP-7A79UUC>
2025-03-30 03:19:38 -0300

    Feat: first commit, testing model implementation

---
+++ app.py
@@ -0,0 +1,35 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+model_name = "tiiuae/Falcon3-7B-Instruct-GPTQ-Int4"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "Hello there, can you give me a summary of the latest news in technology?"
+
+messages = [
+    {"role": "system", "content": "You are a helpful friendly assistant Falcon3 from TII, try to follow instructions as much as possible."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=1024
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(response)
